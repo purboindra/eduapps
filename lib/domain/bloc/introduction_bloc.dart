1d7a2c03d60692671a53a35837c3f103242efe3f
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-14 21:41:49 +0700

    introduction bloc

---
+++ lib/domain/bloc/introduction_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:education_app/app/utils/app_print.dart';
+import 'package:education_app/domain/bloc/base_bloc.dart';
+import 'package:education_app/domain/event/introduction_event.dart';
+import 'package:education_app/domain/repositories/introduction_repository.dart';
+import 'package:education_app/domain/state/introduction_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class IntroductionBloc extends BaseBloc<IntroductionEvent, IntroductionState> {
+  IntroductionBloc(this.introductionRepository)
+      : super(IntroductionInitialState()) {
+    on<GetAllCourseEvent>(_handleGetAllCourse);
+    // on<ChooseProgrammingLanguageEvent>(_handleTapChooseProgrammingLanguage);
+  }
+
+  // void _handleTapChooseProgrammingLanguage(
+  //     ChooseProgrammingLanguageEvent event, Emitter<IntroductionState> emit) {
+  //   emit(ProgrammingLanguageListState([event.course]));
+  // }
+
+  void _handleGetAllCourse(
+      GetAllCourseEvent event, Emitter<IntroductionState> emit) async {
+    emit(LoadingGetAllCourseState());
+    try {
+      final response = await introductionRepository.getAllCourse();
+      emit(SuccessGetAllCourseState(response));
+    } catch (e) {
+      AppPrint.debugPrint("ERROR GET ALL COURSE $e");
+      emit(IntroductionInitialState());
+    }
+  }
+
+  final IntroductionRepository introductionRepository;
+}
