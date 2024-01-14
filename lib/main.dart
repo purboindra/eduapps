ea55c1e7150f86df549d919c0ac80c4a6199eac6
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-14 13:17:39 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:education_app/app/route/router.dart';
 import 'package:education_app/app/utils/colors.dart';
+import 'package:education_app/app_bloc.dart';
+import 'package:education_app/data/repositories/auth_repository_impl.dart';
+import 'package:education_app/dependencies_injection.dart/dependency_injection.dart';
+import 'package:education_app/domain/bloc/auth_bloc.dart';
 import 'package:education_app/domain/cubit/main_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,33 +15,48 @@ void main() async {
 
   await dotenv.load(fileName: ".env");
 
+  await DependencyInject.setup();
+
   await Supabase.initialize(
     url: dotenv.env["SUPABASE_URL"] ?? "",
     anonKey: dotenv.env["SUPABASE_ANON_KEY"] ?? "",
   );
 
-  runApp(const MyApp());
+  Bloc.observer = AppBlocObserver();
+
+  final authRepositoryImpl = AuthRepositoryImpl();
+
+  runApp(MyApp(
+    authRepositoryImpl: authRepositoryImpl,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({required AuthRepositoryImpl authRepositoryImpl, super.key})
+      : _authRepositoryImpl = authRepositoryImpl;
+
+  final AuthRepositoryImpl _authRepositoryImpl;
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => MainCubit(),
-        ),
-      ],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            background: AppColors.backgroundColor,
+    return RepositoryProvider.value(
+      value: _authRepositoryImpl,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => AuthBloc(_authRepositoryImpl)),
+          BlocProvider(
+            create: (_) => MainCubit(),
           ),
+        ],
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+              background: AppColors.backgroundColor,
+            ),
+          ),
+          routerConfig: AppRouter.router,
         ),
-        routerConfig: AppRouter.router,
       ),
     );
   }
