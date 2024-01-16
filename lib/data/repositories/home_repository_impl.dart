68c1ac22891a80136a84fdfa5b0474959d78c561
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-17 05:24:03 +0700

    get all instituion and get all course

--- lib/data/repositories/home_repository_impl.dart
+++ lib/data/repositories/home_repository_impl.dart
@@ -1,3 +1,6 @@
+import 'package:education_app/app/utils/app_print.dart';
+import 'package:education_app/data/entities/course_entity.dart';
+import 'package:education_app/data/entities/institution_entity.dart';
 import 'package:education_app/data/entities/teacher_entity.dart';
 import 'package:education_app/domain/repositories/home_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,9 +11,54 @@ class HomeRepositoryImpl implements HomeRepository {
     List<TeacherEntity> teachers = [];
     final response =
         await Supabase.instance.client.from("teachers").select("*");
+    // final responsA = await Supabase.instance.client
+    //     .from("user")
+    //     .select("courses")
+    //     .eq("uid", "374b32a0-ec7d-42fd-971f-cc2c1feed91b");
+    // for (final a in responsA) {
+    //   for (final b in a["courses"]) {
+    //     AppPrint.debugPrint("APAIUT $b");
+    //     final responsA = await Supabase.instance.client
+    //         .from("quiz")
+    //         .select("*")
+    //         .eq("course_id", "$b");
+    //     for (final s in responsA) {
+    //       AppPrint.debugPrint("QUIZ $s");
+    //     }
+    //   }
+    //   AppPrint.debugPrint("RESPONSE A ${a["courses"]}");
+    // }
     for (final teacher in response) {
       teachers.add(TeacherEntity.fromJson(teacher));
     }
     return teachers;
+  }
+
+  @override
+  Future<List<InstitutionEntity>> getAllInstitutions() async {
+    List<InstitutionEntity> tempInstitution = [];
+    final response =
+        await Supabase.instance.client.from("institution").select("*");
+
+    for (final institution in response) {
+      tempInstitution.add(InstitutionEntity.fromJson(institution));
+    }
+    return tempInstitution;
+  }
+
+  @override
+  Future<List<CourseEntity>> getAllCourse(List<int> courseIds) async {
+    List<CourseEntity> tempCourse = [];
+    for (final courseId in courseIds) {
+      final data = await Supabase.instance.client
+          .from("course")
+          .select("*")
+          .eq("id", courseId.toString());
+      for (final course in data) {
+        AppPrint.debugPrint("GET COURSE $course");
+        tempCourse.add(CourseEntity.fromJson(course));
+      }
+    }
+    return tempCourse;
   }
 }
