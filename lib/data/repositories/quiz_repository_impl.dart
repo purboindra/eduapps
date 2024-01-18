dcd9b097469ead6b76790fb3b441b490120451ce
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-18 10:36:55 +0700

    update logic submit quiz

--- lib/data/repositories/quiz_repository_impl.dart
+++ lib/data/repositories/quiz_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:education_app/data/entities/quiz_entity.dart';
+import 'package:education_app/data/entities/quiz_result_entity.dart';
 import 'package:education_app/domain/repositories/quiz_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -17,8 +18,18 @@ class QuizRepositoryImplement implements QuizRepository {
   }
 
   @override
-  Future<void> submitQuiz(List<Map<String, dynamic>> body, int courseId) async {
+  Future<void> submitQuiz(List<QuizResultEntity> body, int courseId) async {
     final user = Supabase.instance.client.auth.currentUser;
+
+    int totalScore = 0;
+
+    final correctAnswer = body.where((element) => element.score != 0).toList();
+
+    for (final score in correctAnswer) {
+      totalScore += score.score!;
+    }
+
+    List<Map<String, dynamic>> currentQuiz = [];
 
     final getQuiz = await Supabase.instance.client
         .from("user")
@@ -26,15 +37,47 @@ class QuizRepositoryImplement implements QuizRepository {
         .eq("uid", user!.id)
         .select("quiz");
 
-    getQuiz[0]["quiz"]
-        ?.removeWhere((element) => element["course_id"] == courseId);
+    final List<dynamic>? quizData = getQuiz[0]["quiz"] ?? [];
 
-    final existingQuiz = getQuiz[0]["quiz"];
+    if (quizData!.isNotEmpty) {
+      final Map<String, dynamic>? convertQuizToMap = quizData[0];
+      final List<dynamic>? quizList = convertQuizToMap?["quiz"] ?? [];
+      currentQuiz = List<Map<String, dynamic>>.from(quizList ?? []);
+    }
 
-    final mergeQuiz = [...?existingQuiz, ...body];
+    // CONDITION IF USER FIRST TIME SUBMIT QUIZ
+    if (getQuiz[0]["quiz"] != null) {
+      currentQuiz =
+          List<Map<String, dynamic>>.from(getQuiz[0]["quiz"][0]["quiz"] ?? []);
+    }
+
+    final checkExistingQuiz = currentQuiz.indexWhere((element) =>
+        element.isNotEmpty &&
+        element["course_id"] != null &&
+        element["course_id"] == courseId);
+
+    if (checkExistingQuiz == -1) {
+      // NOT FOUND
+      currentQuiz.add({
+        "quiz": body,
+        "course_id": courseId,
+      });
+    } else {
+      currentQuiz[checkExistingQuiz] = {
+        "course_id": courseId,
+        "quiz": body.map((e) => e.toJson()).toList(),
+      };
+    }
 
     await Supabase.instance.client.from("user").update({
-      "quiz": mergeQuiz,
+      "quiz": [
+        {
+          "course_id": courseId,
+          "total_score": totalScore,
+          "correct_answer": correctAnswer.length,
+          "quiz": currentQuiz,
+        },
+      ],
     }).eq("uid", user.id);
   }
 }
