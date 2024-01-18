076fff35c78a8d40729a4ffbe397db161e603291
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-18 17:09:04 +0700

    di course repository

--- lib/dependencies_injection.dart/dependency_injection.dart
+++ lib/dependencies_injection.dart/dependency_injection.dart
@@ -1,8 +1,10 @@
 import 'package:education_app/data/repositories/auth_repository_impl.dart';
+import 'package:education_app/data/repositories/course_repository_impl.dart';
 import 'package:education_app/data/repositories/home_repository_impl.dart';
 import 'package:education_app/data/repositories/introduction_repository_impl.dart';
 import 'package:education_app/data/repositories/quiz_repository_impl.dart';
 import 'package:education_app/domain/repositories/auth_repository.dart';
+import 'package:education_app/domain/repositories/course_repository.dart';
 import 'package:education_app/domain/repositories/home_repository.dart';
 import 'package:education_app/domain/repositories/introduction_repository.dart';
 import 'package:education_app/domain/repositories/quiz_repository.dart';
@@ -17,5 +19,6 @@ class DependencyInject {
         IntroductionRepositoryImpl());
     getIt.registerSingleton<HomeRepository>(HomeRepositoryImpl());
     getIt.registerSingleton<QuizRepository>(QuizRepositoryImplement());
+    getIt.registerSingleton<CourseRepository>(CourseRepositoryImpl());
   }
 }
