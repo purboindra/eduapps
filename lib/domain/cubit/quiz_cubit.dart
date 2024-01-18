deeac7ebe5ea3cfec51181a8cf7138fad7c20fcb
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-18 14:44:48 +0700

    add stack trace submit quiz

--- lib/domain/cubit/quiz_cubit.dart
+++ lib/domain/cubit/quiz_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:education_app/app/utils/app_print.dart';
+import 'package:education_app/data/entities/quiz_result_entity.dart';
 import 'package:education_app/domain/repositories/quiz_repository.dart';
 import 'package:education_app/domain/state/quiz_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,13 +13,13 @@ class QuizCubit extends Cubit<QuizState> {
     emit(SelectedAnswerState(index));
   }
 
-  void submitQuiz(List<Map<String, dynamic>> body, int courseId) async {
+  void submitQuiz(List<QuizResultEntity> body, int courseId) async {
     emit(LoadingSubmitQuizState());
     try {
       await quizRepository.submitQuiz(body, courseId);
       emit(SuccessSubmitQuizState());
-    } catch (e) {
-      AppPrint.erroLog("ERROR FROM SUBMIT QUIZ $e");
+    } catch (e, st) {
+      AppPrint.erroLog("ERROR FROM SUBMIT QUIZ $e", st: st);
       emit(InitialQuizState());
     }
   }
